ab5b02349b68c23389803e9c35c7cca595dc5ed0
danielgao20 <dygao@usc.edu>
2024-06-05 17:05:00 -0700

    submission

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profilePic from '../assets/profilepic.png'; // Adjust the path
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img 
+                className="profile_image"
+                src={profilePic}
+                alt="Profile Pic"
+              ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Daniel Gao</div>
+              <div className="brief_description">
+                {"Software Developer"}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
